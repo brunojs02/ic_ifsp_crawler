4fba0e97cd93b88e66de1656b5c616f0a961e916
Bruno Santos <bruno.js02@gmail.com>
2017-04-26 13:27:24 -0300

    Agora o caminho do PhantomJS binário é pegado automaticamente

--- webcrawler/src/main/java/br/edu/ifsp/arq/ic/webcrawler/browsers/BrowserPhantomJS.java
+++ webcrawler/src/main/java/br/edu/ifsp/arq/ic/webcrawler/browsers/BrowserPhantomJS.java
@@ -1,17 +1,17 @@
 package br.edu.ifsp.arq.ic.webcrawler.browsers;
 
-import java.io.File;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserPhantomJS {
 	private WebDriver driver;
 	
 	public WebDriver getDriver(){
-		File filePathToPhantom = new File("/home/bruno/workspace-ic-ifsp/drivers/phantomjs/bin/phantomjs");
-		System.setProperty("phantomjs.binary.path", filePathToPhantom.getAbsolutePath());
-		driver = new PhantomJSDriver(); 
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, System.getProperty("user.dir") + "/../drivers/phantomjs/bin/phantomjs");
+		driver = new PhantomJSDriver(caps);
 		return driver;
 	}
 	
